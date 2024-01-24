4cd78597aeb4f86f0113dbf5ae115e5a5c93cabf
Mohammadshuvouup <mohammad.ali.shuvo161@gmail.com>
2024-01-24 12:35:25 +0600

    todo design

--- index.js
+++ index.js
@@ -1,169 +1,103 @@
-let btn = document.querySelectorAll(".btn")
-let btns = document.getElementById("dl")
-console.log("hello",btn);
-btns.innerText = "hekko";
+let add_btn = document.querySelector(".additem");
+let todo_list = document.querySelector(".todo");
+let todoItems = [];
 
- for(let i = 0; i < btn.length; i++){
-    console.log("hello",btn[i]);
-    btn[i].addEventListener('click', function() {
+function taskInput(target) {
+    console.log("target", target);
+    let li = target.closest("li")
+    let nodeList =  document.querySelectorAll('li');
+    let arrayList = Array.from(nodeList);
+    let index = arrayList.indexOf(li);
 
-      // loop your button again to remove all "active"
-      for(let j = 0; j < btn.length; j++){
-        btn[j].classList.remove("active");
-      }
-      // add active class to your clicked button
-      btn[i].classList.add("active");
-    });
+  target.addEventListener("keyup", (e) => {
+    let prevSibling = target.previousElementSibling;
+    if (e.key == "Enter") {
+      target.classList.add("hide");
+      prevSibling.classList.remove("hide");
+      prevSibling.innerHTML = target.value;
 
- }
+        if(index == arrayList.length-1){
+            todoItems.push(target.value)
+        }
+        else{
+            todoItems[index] = target.value;
 
-// Remove falsy value from any array
+        }
 
-// let hello =['Apple',false,'orange',NaN,'banana',null,'chilli',undefined,'hello'];
-// let fruits = hello.filter(Boolean);
-// console.log(fruits);
+      localStorage.setItem("todolist", JSON.stringify(todoItems));
+    }
+  });
+}
 
-// let animals =['cat','tiger','lion','dog']
-// animals.length = 2;
-// console.log(animals)
+window.onload = function () {
+  const cookie = localStorage.getItem("todolist");
+  todoItems = cookie ? JSON.parse(cookie) : [];
 
-// const quote = "i am mohammad ali"
-// console.log(quote.replace(/ali/g, "SHuvo"))
+  todoItems.forEach((element) => {
+    console.log(todoItems);
+    const li = document.createElement("li");
+    li.className = "todolist-item";
+    li.innerHTML = `<span class="text">
+                           ${element}
+                        </span>
+                        <input class="task-field hide" type="text">
+                        <div class="btn">
+                            <span class="editbtn" >Edit</span>
+                            <span class="delete-btn">Delete</span>
+                        </div>`;
+    todo_list.appendChild(li);
+  });
+};
 
-//  const btn = document.querySelectorAll(".btn")
-// console.log("hello",btn);
+function deleteListELement(target) {
+  const li = target.closest("li");
+  let nodeList = document.querySelectorAll("li");
+  let arrayList = Array.from(nodeList);
+  let index = arrayList.indexOf(li);
+  todoItems.splice(index, 1);
+  console.log("arraylist", todoItems);
+  localStorage.setItem("todolist", JSON.stringify(todoItems));
+  li.remove();
+}
 
-for(let i = 0; i < btn.length; i++){
-  console.log("he",btn[i]);
-    btn[i].addEventListener('mouseover', function() {
-      btn[i].classList.add("active");
+function editListElement(target) {
+  let li = target.closest("li");
+  const inputField = li.querySelector(".task-field");
+  inputField.classList.remove("hide")
+  const text = li.querySelector('.text');
+  text.classList.add('hide');
+  inputField.value = text.innerHTML
 
-      for (let j =0; j < btn.length; j++){
-        btn[i].addEventListener('mouseout', function() {
-          btn[i].classList.remove("active");
+}
 
-      });
+document.addEventListener("click", function (e) {
+  const isTaskField = e.target.classList.contains("task-field"); // Or any other selector.
+  const isDelete = e.target.classList.contains("delete-btn");
+  const isEdit = e.target.classList.contains("editbtn");
 
-      }
-
-    });
-
+  if (isEdit) {
+    editListElement(e.target);
   }
 
-// const btn = document.querySelectorAll(".btn")
-// console.log("hello",btn);
+  if (isDelete) {
+    deleteListELement(e.target);
+  }
 
-//     console.log("hello");
-//     btn.addEventListener('submit', function() {
+  if (isTaskField) {
+    taskInput(e.target);
+  }
+});
 
-//     });
+add_btn.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.className = "todolist-item";
+  li.innerHTML = `<span class="text hide">
+                    </span>
+                    <input class="task-field" type="text">
+                    <div class="btn">
+                        <span class="editbtn" >Edit</span>
+                        <span class="delete-btn">Delete</span>
+                    </div>`;
 
-// const textFild = document.querySelector(".text");
-// console.log(textFild);
-// textFild.addEventListener("keypress", function(event) {
-//     if (event.key === "Enter") {
-//         const result = document.querySelector('.result');
-//         result.textContent = `output = ${event.target.value}`;
-//         console.log(event.target.value)
-//     }
-
-// });
-// const textFild = document.querySelector(".text");
-// const textWord = document.querySelector(".result");
-// const textChar = document.querySelector(".result2");
-
-//     const countChar = (str) => {
-//         return typeof (str) ==="string" && str.trim().length
-//     }
-//     const countWord = (str) => {
-//         console.log(str.trim())
-//         return typeof (str) ==="string" && str.trim().split(/\s+/).filter(st => st !="").length
-//     }
-//     textFild.addEventListener("input", () =>{
-
-//          textWord.innerHTML = countWord(textFild.value);
-//          textChar.innerHTML = countChar(textFild.value);
-//     })
-
-// let createCounter = function(n) {
-//   let count = n;
-//   return function() {
-//       count++;
-//       return count-1;
-
-//   };
-// };
-
-// const counter = createCounter(10)
-// counter() // 10
-// counter() // 11
-// counter() // 12
-// console.log("llll",counter())
-// console.log("llll",counter())
-// console.log("llll",counter())
-
-// (() => {
-//   const SUBMIT_URL =
-//     'https://www.greatfrontend.com/api/questions/contact-form';
-
-//   const $form = document.querySelector('form');
-//   $form.addEventListener('submit', async (event) => {
-//     event.preventDefault();
-
-//     if ($form.action !== SUBMIT_URL) {
-//       alert('Incorrect form action value');
-//       return;
-//     }
-
-//     if ($form.method.toLowerCase() !== 'post') {
-//       alert('Incorrect form method value');
-//       return;
-//     }
-
-//     try {
-//       const formData = new FormData($form);
-//       const response = await fetch(SUBMIT_URL, {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//           name: formData.get('name'),
-//           email: formData.get('email'),
-//           message: formData.get('message'),
-//         }),
-//       });
-
-//       const text = await response.text();
-//       alert(text);
-//     } catch (_) {
-//       alert('Error submitting form!');
-//     }
-//   });
-// })();
-
-
-
-
-// let items = document.querySelectorAll("li");
-
-// let searchInput = document.querySelector(".search-list");
-// const filterSearch = () =>{
-//   items.forEach(item => {
-//     if(item.textContent.toLowerCase().includes(searchInput.value.toLowerCase())){
-//       item.classList.remove("hide");
-//     } else {
-//       item.classList.add("hide")
-//     }
-//   });
-// }
-
-// searchInput.addEventListener('keyup',(e)=>{
-//   let inputValue = e.target.value;
-//   console.log(inputValue)
-//   filterSearch()
- 
-// })
-
-// console.log("hello", searchInput);
-
+  todo_list.appendChild(li);
+});
